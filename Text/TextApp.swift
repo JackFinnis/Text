97cf1e99f75cb4b6592c4a56c2876c78300cb427
Jack Finnis <finnis.j@etoncollege.org.uk>
2022-08-28 20:36:38 +0100

    Initial Commit

---
+++ Text/TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextApp.swift
+//  Text
+//
+//  Created by Jack Finnis on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
