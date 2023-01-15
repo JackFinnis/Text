4926dec1e9a96f9a0f25e12136d21db355706a33
Jack Finnis <jack.finnis@icloud.com>
2023-01-15 20:04:20 +0000

    Counts how many words

--- Text/TextApp.swift
+++ Text/TextApp.swift
@@ -13,6 +13,26 @@ struct TextApp: App {
         WindowGroup {
             ContentView()
         }
-        .windowStyle(.hiddenTitleBar)
+        .defaultSize(width: 300, height: 200)
+    }
+}
+
+struct ContentView: View {
+    @State var text = ""
+    @State var monospaced = true
+    
+    var words: Int {
+        NSSpellChecker.shared.countWords(in: text, language: nil)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            TextEditor(text: $text.animation())
+                .font(monospaced ? .system(size: 15).monospaced() : .system(size: 15))
+                .navigationTitle(text.isEmpty ? "" : (words == 1 ? "1 word" : "\(words) words"))
+                .toolbar {
+                    Toggle("Code", isOn: $monospaced.animation())
+                }
+        }
     }
 }
