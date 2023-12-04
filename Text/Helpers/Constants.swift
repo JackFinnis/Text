20bf1f0a05b1a86ec15ae3b147033d78627e169b
Jack Finnis <jack.finnis@icloud.com>
2023-12-04 21:24:42 +0000

    More welcome ideas

--- Text/Helpers/Constants.swift
+++ Text/Helpers/Constants.swift
@@ -14,10 +14,14 @@ struct Constants {
     static let verticalPadding = 8.0
     static let horizontalPadding = 16.0
     static let welcomeMessage = """
+Jot down ideas
 Save important links
 Easily create calendar events
-Jot down ideas, addresses, phone numbers
 Take notes in a meeting or lecture
 Check the character-count of a Tweet
+Look up a word in the dictionary
+Translate a scentence to any language
+Scan text from a document
+Convert text to speech
 """
 }
